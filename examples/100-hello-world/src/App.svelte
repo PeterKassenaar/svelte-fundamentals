0a66b910bcf9b612786ac67d72005e51ec490a99
PeterKassenaar <info@kassenaar.com>
2026-02-27 10:02:41 +0100

    feat: add example 100-hello-world

---
+++ examples/100-hello-world/src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+    import Header from "./lib/Header.svelte";
+    let name = "Peter";
+    let age = 12;
+</script>
+
+<!--HTML template-->
+<div>
+    <h1>Svelte app</h1>
+    <!--    Traditional notation-->
+    <Header name={name} age={age}/>
+    <!-- Shorthand notation,
+        when key and value are the same-->
+    <Header {name} {age}/>
+</div>
+
+<style>
+    div {
+        border: 1px solid red;
+        padding: 10px;
+    }
+</style>
---
+++ examples/100-hello-world/src/lib/Header.svelte
@@ -0,0 +1,15 @@
+<!--Header.svelte-->
+<script lang="ts">
+    let {name, age} = $props();
+</script>
+
+<div>
+    <h2>Hello, my name is { name}</h2>
+    <p>And I am {age} years old</p>
+</div>
+<style>
+    div {
+        border: 1px solid #ccc;
+        padding: 4px;
+    }
+</style>
