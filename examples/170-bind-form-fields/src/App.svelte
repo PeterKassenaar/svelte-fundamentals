3e0e931ca65b734e0b027371c9f896a9d76ec92f
PeterKassenaar <info@kassenaar.com>
2026-03-16 08:16:05 +0100

    feat: add dropdown to Form binding examples

--- examples/170-bind-form-fields/src/App.svelte
+++ examples/170-bind-form-fields/src/App.svelte
@@ -1,15 +1,17 @@
 <script lang="ts">
 
+    // Examples in this mini project. Replace the component on line 13 to use the other examples.
     import TextInputExample from "./lib/TextInputExample.svelte";
     import CheckboxExample from "./lib/CheckboxExample.svelte";
     import RadioButtonExample from "./lib/RadioButtonExample.svelte";
     import CheckboxGroupExample from "./lib/CheckboxGroupExample.svelte";
     import TextAreaExample from "./lib/TextAreaExample.svelte";
+    import SelectExample from "./lib/SelectExample.svelte";
 </script>
 <!--HTML template-->
 <div>
     <h2>A small Svelte app showing different <code>bind:</code> options for form fields</h2>
-    <TextInputExample />
+    <TextInputExample/>
 </div>
 
 <style>
---
+++ examples/170-bind-form-fields/src/lib/SelectExample.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    // Example of a <select>/dropdown component
+    let selectedCountry = 'NL';
+
+    const countries = [
+        { code: 'NL', name: 'Netherlands' },
+        { code: 'DE', name: 'Germany' },
+        { code: 'FR', name: 'France' },
+        { code: 'ES', name: 'Spain' }
+    ];
+</script>
+
+<h3>Select Country from dropdown</h3>
+<p>
+    The <code>code</code> property represents the country code,
+    which is used for identification purposes.
+</p>
+<p>Selected country code: <strong>{selectedCountry}</strong></p>
+<select bind:value={selectedCountry}>
+    {#each countries as country}
+        <option value={country.code}>
+            {country.name}
+        </option>
+    {/each}
+</select>
+
